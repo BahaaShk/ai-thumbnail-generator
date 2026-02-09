65eaf58c4d1bfd1f9c2b234529e47a0cc15183c9
BahaaShk <bahaa.shk@gmail.com>
2026-02-09 23:56:16 +0200

    Add SoftBackdrop and structure MyGeneration page layout

--- frontend/src/pages/MyGeneration.tsx
+++ frontend/src/pages/MyGeneration.tsx
@@ -1,7 +1,18 @@
+import SoftBackdrop from "../components/SoftBackdrop"
 
 const MyGeneration = () => {
   return (
-    <div>MyGeneration</div>
+    <>
+    <SoftBackdrop />
+    <div className=" mt-32 min-h-screen px-6 md:px-16 lg:px-24 xl:px-32">
+      {/* header  */}
+
+      <div className=" mb-8">
+        <h1 className=" text-2xl font-bold text-zinc-200">My Generations</h1>
+        <p className=" text-sm mt-1 text-zinc-400">View and manage your AI-generated thumbnails</p>
+      </div>
+    </div>
+    </>
   )
 }
 
