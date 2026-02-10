1995e29b3d9c130ffc08be77d3f897f73108ea0b
BahaaShk <bahaa.shk@gmail.com>
2026-02-11 00:52:05 +0200

    Refactor MyGeneration component to include thumbnail fetching logic and loading state

--- frontend/src/pages/MyGeneration.tsx
+++ frontend/src/pages/MyGeneration.tsx
@@ -1,11 +1,30 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import SoftBackdrop from "../components/SoftBackdrop"
-import type { IThumbnail } from "../assets/assets"
+import { dummyThumbnails, type IThumbnail } from "../assets/assets"
 
 const MyGeneration = () => {
 
 const [thumbnails, setThumbnails] = useState<IThumbnail>([])
 const [loading, setLoading] = useState(false)
+
+const fetchThumbnails = async () => {
+setThumbnails(dummyThumbnails as unknown as IThumbnail[])
+setLoading(false)
+}
+
+const handleDownload = (image_url: string) => {
+window.open(image_url,'_blank')
+}
+
+const handleDelete = async (id: string) => {
+  console.log(id);
+  
+}
+
+useEffect(() => {
+fetchThumbnails()
+}, [])
+
 
   return (
     <>
@@ -17,6 +36,17 @@ const [loading, setLoading] = useState(false)
         <h1 className=" text-2xl font-bold text-zinc-200">My Generations</h1>
         <p className=" text-sm mt-1 text-zinc-400">View and manage your AI-generated thumbnails</p>
       </div>
+
+      {/* loading */}
+      {
+        loading && (
+          <div className=" grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
+{Array.from({length:6}).map((_,i) => (
+  <div className=" rounded-2xl bg-white/5 border border-white/10 animate-pulse h-[260px]" key={i}/>
+))}
+          </div>
+        )
+      }
     </div>
     </>
   )
