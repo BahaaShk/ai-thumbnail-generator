f77e904dcadf5d63b7dda896f0734bbdb03f7f1a
BahaaShk <bahaa.shk@gmail.com>
2026-03-18 22:40:12 +0200

    refactor axios configuration and improve code formatting for clarity

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,12 +1,17 @@
 import { createRoot } from 'react-dom/client'
 import App from './App.js'
 import { BrowserRouter } from 'react-router-dom'
-import {AuthProvider} from './context/AuthContext'
+import { AuthProvider } from './context/AuthContext'
+import axios from 'axios'
+
+// Global axios config — applies to every request automatically
+axios.defaults.withCredentials = true;
+axios.defaults.baseURL = import.meta.env.VITE_API_URL;
 
 createRoot(document.getElementById('root')!).render(
     <BrowserRouter>
-    <AuthProvider>
-        <App />
-    </AuthProvider>
+        <AuthProvider>
+            <App />
+        </AuthProvider>
     </BrowserRouter>,
 )
